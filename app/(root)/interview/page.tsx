7f24cca454fa5bf13399dc350b9d7a733916fe90
Arghadeep <arghadeeps07@gmail.com>
2025-05-20 23:20:58 +0530

    Interview page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Agent userName="Arghadeep" userId="user1" type="generate"  />
+    </>
+  )
+}
+
+export default page
